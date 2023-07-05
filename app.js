3059a4c285f80f2d16fc95e0dfad92e00fed43ad
Leonardo Mariño <126926390+lmarino15@users.noreply.github.com>
2023-07-05 10:21:53 -0500

    .

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ function cambiarIdioma() {
     const p = document.getElementById('P');
     if (traducir) {
         h4.innerHTML = "Developer Junior";
-        p.innerHTML = "This is my cover letter where you can find icons redirecting to my GitHub portfolio. Possesses knowledge of HTML, CSS, JavaScript and Csharp.";
+        p.innerHTML = "This is my cover letter where you can find the icon that redirects to my GitHub portfolio. I have knowledge of HTML, CSS, JavaScript and Csharp.";
         traducir = false;
     } else {
         h4.innerHTML = "Programador Junior";
-        p.innerHTML = "Esta es mi carta de presentación donde podrás encontrar iconos redireccionando a mi portafolio de GitHub. Posee conocimiento de HTML, CSS, JavaScript y Csharp.";
+        p.innerHTML = "Esta es mi carta de presentación donde podrás encontrar el ícono que redirecciona a mi portafolio de GitHub. Poseo conocimiento de HTML, CSS, JavaScript y Csharp.";
         traducir = true;
     }
 }
